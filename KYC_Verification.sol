35fdb4895dd5afd5fbeee6e1549f2c8dd55cec3a
Sai Reddy <40784517+saiobulreddy@users.noreply.github.com>
2021-05-03 13:47:12 +0530

    Update KYC_Verification.sol
    updated with proper validation

--- KYC_Verification.sol
+++ KYC_Verification.sol
@@ -6,7 +6,7 @@ contract KYC_Verification{
     address admin;
     uint256 totalBanks;
     
-    constructor() public {
+    constructor() {
         admin = msg.sender;
         totalBanks = 0;
     }
@@ -50,25 +50,41 @@ contract KYC_Verification{
     // this is an interface to varify the incoming request is from valid bank or not
     modifier isValidBank {
         require(
-            banks[msg.sender].ethAddress != address(0) ||
-            banks[msg.sender].ethAddress == 0x0000000000000000000000000000000000000000 ||
-            bytes(banks[msg.sender].name).length == 0,
+            banks[msg.sender].ethAddress != 0x0000000000000000000000000000000000000000 &&
+            bytes(banks[msg.sender].name).length > 0 &&
+            bytes(banks[msg.sender].regNumber).length > 0,
             "Not a valid bank account.");
+        _;
+    }
+    
+    modifier isCustomerExist(string memory _customerName){
+        require(bytes(customers[_customerName].userName).length > 0, "Customer not exist with this name.");
         _;
     }
     
     modifier isBankExist(address _bankAddress) {
         require(
-            _bankAddress != address(0) ||
-            _bankAddress == 0x0000000000000000000000000000000000000000,
-            // banks[_bankAddress].name == null,
+            _bankAddress != 0x0000000000000000000000000000000000000000 &&
+            bytes(banks[_bankAddress].name).length > 0 &&
+            bytes(banks[_bankAddress].regNumber).length > 0,
             "Bank dosen't exist with provided details."
             );
         _;
     }
     
+    modifier ispermitedToVote {
+        require(banks[msg.sender].isAllowedToVote == true, "Bank have no permission to cast vote.");
+        _;
+    }
+    
     // function to add bank by onlyAdmin
     function addBank(string memory _bankName, address _bankAddress, string memory _regNumber) public onlyAdmin {
+        require(_bankAddress != 0x0000000000000000000000000000000000000000, "Can't be an empty address.");
+        require(
+            keccak256(abi.encodePacked(banks[_bankAddress].name)) != keccak256(abi.encodePacked(_bankName)) &&
+            keccak256(abi.encodePacked(banks[_bankAddress].regNumber)) != keccak256(abi.encodePacked(_regNumber)),
+            "Bank alredy exist with provided details."
+            );
         banks[_bankAddress].name = _bankName;
         banks[_bankAddress].ethAddress = _bankAddress;
         banks[_bankAddress].regNumber = _regNumber;
@@ -78,11 +94,11 @@ contract KYC_Verification{
     }    
     
     // function to retrive bank details
-    function viewBankDetails(address _bankAddress) public view returns (Bank memory) {
+    function viewBankDetails(address _bankAddress) public isBankExist(_bankAddress) view returns (Bank memory) {
         return banks[_bankAddress];
     }    
     
-    // function viewBankDetails(address _bankAddress) public view returns (string memory, address, uint256, uint256, bool, string memory) {
+    // function viewBankDetails(address _bankAddress) public isBankExist(_bankAddress) view returns (string memory, address, uint256, uint256, bool, string memory) {
     //   return (
         //   banks[_bankAddress].name,
         //   banks[_bankAddress].ethAddress, 
@@ -111,16 +127,16 @@ contract KYC_Verification{
 
     // to report any bank by oter banks
     function reportBank(address _bankAddress, string memory _bankName ) public isValidBank isBankExist(_bankAddress) {
-        require(bytes(banks[_bankAddress].name).length != bytes(_bankName).length, "Bank dosen't existed with the provided name.");
+        require(keccak256(abi.encodePacked(banks[_bankAddress].name)) == keccak256(abi.encodePacked(_bankName)), "Bank dosen't matched with provided name.");
         banks[_bankAddress].complaintsReported += 1;
         if(banks[_bankAddress].complaintsReported > totalBanks/3){
             banks[_bankAddress].isAllowedToVote = false;
         }
     } 
     
-    // to rise a KYC request for custmor by bank's
+    // to rise a KYC request for customer by bank's
     function addRequest(string memory _customerName, string memory _customerData) public isValidBank {
-        require(bytes(kycRequest[_customerName].customerName).length == bytes(_customerName).length,"Customer alredy exists with this name.");
+        require(keccak256(abi.encodePacked(kycRequest[_customerName].customerName)) != keccak256(abi.encodePacked(_customerName)),"Customer alredy exists with this name.");
         kycRequest[_customerName].customerName = _customerName;
         kycRequest[_customerName].customerData = _customerData;
         kycRequest[_customerName].bankAddress = msg.sender;
@@ -128,17 +144,18 @@ contract KYC_Verification{
         this.addCustomer(_customerName, _customerData);
     }
     
-    // to cancle the KYC request of a custmor
+    // to cancle the KYC request of a customer
     function removeRequest(string memory _customerName) public isValidBank {
-        require(kycRequest[_customerName].bankAddress != msg.sender,"Not a valid bank to remove the request.");
-        require(customers[_customerName].kycStatus == true, "Customer KYC approved, can't be remove.");
+        require(kycRequest[_customerName].bankAddress == msg.sender,"Customer not exist in this bank.");
+        require(bytes(customers[_customerName].userName).length > 0,"Customer not exist in this bank.");
+        require(customers[_customerName].kycStatus == false, "Customer KYC approved, can't be remove.");
         delete kycRequest[_customerName];
+        delete customers[_customerName];
     }
     
     // to add new customers into the system
-    function addCustomer(string memory _userName, string memory _customerData) public isValidBank isBankExist(msg.sender) {
-        // require(keccak256(abi.encodePacked(customers[_userName].userName)) == keccak256(abi.encodePacked(_userName)), "Customer alredy exists with this name.");
-        // require(customers[_userName].userName == _userName, "Customer alredy exists with this name.");
+    function addCustomer(string memory _userName, string memory _customerData) public  {
+        require(bytes(customers[_userName].userName).length == 0, "Customer alredy exists with this name.");
         customers[_userName].userName = _userName;
         customers[_userName].data = _customerData;
         customers[_userName].Upvotes = 0;
@@ -147,21 +164,21 @@ contract KYC_Verification{
         customers[_userName].bank = msg.sender;
     }
     
-    // to modify or re-update the custmor data for new KYC request
-    function modifyCustomer(string memory _userName, string memory _newcustomerData) public isValidBank {
-        require(customers[_userName].bank != msg.sender, "Not an authorisd bank.");
+    // to modify or re-update the customer data for new KYC request
+    function modifyCustomer(string memory _userName, string memory _newcustomerData) public isValidBank isCustomerExist(_userName) {
+        require(customers[_userName].bank == msg.sender, "Not an authorisd bank to modify customer.");
         this.removeRequest(_userName);
         customers[_userName].data = _newcustomerData;
         customers[_userName].Upvotes = 0;
         customers[_userName].Downvotes = 0;
     } 
     
-    // to view custmor details 
-    function viewCustomer(string memory _userName) public view returns (Customer memory) {
+    // to view customer details 
+    function viewCustomer(string memory _userName) public isCustomerExist(_userName) view returns (Customer memory) {
         return customers[_userName];
     }
     
-    // function viewCustomer(string memory _userName) public view returns (string memory, string memory, address, bool, uint, uint) {
+    // function viewCustomer(string memory _userName) public isCustomerExist(_userName) view returns (string memory, string memory, address, bool, uint, uint) {
     //     return (
     //         customers[_userName].userName,
     //         customers[_userName].data,
@@ -172,24 +189,17 @@ contract KYC_Verification{
     //         );  
     // }
     
-    // to vote custmora to approve their KYC docs
-    function upvoteCustomer(string memory _customerName) public isValidBank {
+    // to vote customer to approve their KYC docs
+    function upvoteCustomer(string memory _customerName) public isValidBank ispermitedToVote isCustomerExist(_customerName) {
         customers[_customerName].Upvotes += 1;
         if(customers[_customerName].Upvotes >= totalBanks/2 && customers[_customerName].Downvotes <= totalBanks/3){
             customers[_customerName].kycStatus = true;
         }
     }    
     
-    // to vote custmor to declain their KYC docs
-    function downvoteCustomer(string memory _customerName) public isValidBank {
+    // to vote customer to declain their KYC docs
+    function downvoteCustomer(string memory _customerName) public isValidBank ispermitedToVote isCustomerExist(_customerName) {
         customers[_customerName].Downvotes += 1;
     }    
 
-    // queries to know about
-    // how to compare two strings
-    // how to get all the element keys in mapping
-    // how to get the count of elements present in mapping
-    // how to compare address
 }    
-
-
